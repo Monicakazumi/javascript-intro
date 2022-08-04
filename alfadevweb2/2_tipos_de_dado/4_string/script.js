8103ec6ebc6757595901d85925aa138bfeba090d
Monicakazumi <106854565+Monicakazumi@users.noreply.github.com>
2022-08-03 22:10:51 -0300

    Add files via upload
    Tipos de dados

---
+++ alfadevweb2/2_tipos_de_dado/4_string/script.js
@@ -0,0 +1,8 @@
+console.log("Meu nome é Mônica");
+console.log('Sou estudante');
+console.log(`Isso é um teste`);
+
+
+console.log(typeof("Meu nome é Mônica"));
+console.log(typeof('Sou estudante'));
+console.log(typeof(`Isso é um teste`));
---
+++ alfadevweb2/2_tipos_de_dado/8_boolean_operadores/script.js
@@ -0,0 +1,6 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 2);
+console.log(!(5 > 3 || 3 == 2));
+console.log(3 === '3' && "alan" == "ALAN");
+console.log(true && true);
+console.log(!(true && true));
