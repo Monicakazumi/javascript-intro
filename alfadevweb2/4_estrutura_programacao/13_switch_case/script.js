f2c101429b561c5d4f5f4b875ba5c3c3ba25d718
Monicakazumi <106854565+Monicakazumi@users.noreply.github.com>
2022-09-28 22:18:45 -0300

    Add files via upload

---
+++ alfadevweb2/4_estrutura_programacao/13_switch_case/script.js
@@ -0,0 +1,11 @@
+nome = "Monica";
+switch(nome){
+    case "Daniel":
+        console.log("Bem vindo Daniel");
+        break;
+    case "João":
+        console.log("Bem vindo João");
+        break;
+    default:
+    console.log("Nome não encontrado!");
+}
